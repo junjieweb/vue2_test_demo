1c19fd38e017b411e358f791b7de15145161ced4
junjieweb <297901657@qq.com>
2022-03-07 02:32:20 +0800

    add code

--- 22_插槽/1_src_默认插槽/App.vue
+++ 22_插槽/1_src_默认插槽/App.vue
@@ -2,39 +2,22 @@
   <div class="container">
     <Category title="美食">
       <img
-        slot="center"
         src="https://cdn.pixabay.com/photo/2016/10/23/09/37/fried-rice-1762493_1280.jpg"
         alt="美食"
       />
-      <div class="foot" slot="footer">
-        <a href="https://www.baidu.com">更多美食</a>
-      </div>
     </Category>
 
     <Category title="游戏">
-      <ul slot="center">
+      <ul>
         <li v-for="(game, index) in games" :key="index">{{ game }}</li>
       </ul>
-      <div class="foot" slot="footer">
-        <a href="https://www.baidu.com">单机游戏</a>
-        <a href="https://www.baidu.com">网络游戏</a>
-      </div>
     </Category>
-
+    
     <Category title="电影">
       <video
-        slot="center"
         controls
         src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
       ></video>
-      <template v-slot:footer>
-        <div class="foot">
-          <a href="https://www.baidu.com">经典</a>
-          <a href="https://www.baidu.com">热门</a>
-          <a href="https://www.baidu.com">推荐</a>
-        </div>
-        <h4>欢迎来观影</h4>
-      </template>
     </Category>
   </div>
 </template>
@@ -60,14 +43,5 @@ export default {
 .container {
   display: flex;
   justify-content: space-around;
-}
-.foot {
-  text-align: center;
-}
-a {
-  padding: 5px;
-}
-h4 {
-  text-align: center;
 }
 </style>
